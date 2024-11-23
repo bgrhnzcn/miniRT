47e43bedc11e1849c6ed9401eab97fd1fa530b8a
bgrhnzcn <bugrahanozcan16@gmail.com>
2024-11-24 01:18:05 +0300

    Added debug functions for frame time.

--- src/debug.c
+++ src/debug.c
@@ -6,7 +6,7 @@
 /*   By: bgrhnzcn <bgrhnzcn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 23:36:19 by bgrhnzcn          #+#    #+#             */
-/*   Updated: 2024/11/23 23:49:58 by bgrhnzcn         ###   ########.fr       */
+/*   Updated: 2024/11/24 01:11:28 by bgrhnzcn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,89 +15,89 @@
 static void	print_debug_light(t_light *light)
 {
 	printf("Light:\n");
-	printf("\tBrightness: %f\n", light->brightness);
+	printf("\tBrightness:\t%f\n", light->brightness);
 	printf("\tPosition:\n");
-	printf("\t\tX: %f\n", light->position.x);
-	printf("\t\tY: %f\n", light->position.y);
-	printf("\t\tZ: %f\n", light->position.z);
+	printf("\t\tX:\t%f\n", light->position.x);
+	printf("\t\tY:\t%f\n", light->position.y);
+	printf("\t\tZ:\t%f\n", light->position.z);
 	printf("\tColor:\n");
-	printf("\t\tRed: %u\n", light->color.red);
-	printf("\t\tGreen: %u\n", light->color.green);
-	printf("\t\tBlue: %u\n", light->color.blue);
+	printf("\t\tRed:\t%u\n", light->color.red);
+	printf("\t\tGreen:\t%u\n", light->color.green);
+	printf("\t\tBlue:\t%u\n", light->color.blue);
 }
 
 static void	print_debug_camera(t_camera *camera)
 {
 	printf("Camera:\n");
-	printf("\tFOV: %f\n", camera->fov);
+	printf("\tFOV:\t\t%f\n", camera->fov);
 	printf("\tPosition:\n");
-	printf("\t\tX: %f\n", camera->position.x);
-	printf("\t\tY: %f\n", camera->position.y);
-	printf("\t\tZ: %f\n", camera->position.z);
+	printf("\t\tX:\t%f\n", camera->position.x);
+	printf("\t\tY:\t%f\n", camera->position.y);
+	printf("\t\tZ:\t%f\n", camera->position.z);
 	printf("\tOrientation:\n");
-	printf("\t\tX: %f\n", camera->orientation.x);
-	printf("\t\tY: %f\n", camera->orientation.y);
-	printf("\t\tZ: %f\n", camera->orientation.z);
+	printf("\t\tX:\t%f\n", camera->orientation.x);
+	printf("\t\tY:\t%f\n", camera->orientation.y);
+	printf("\t\tZ:\t%f\n", camera->orientation.z);
 }
 
 static void	print_debug_ambient(t_ambient *ambient)
 {
 	printf("Ambient:\n");
-	printf("\tStrength: %f\n", ambient->strength);
+	printf("\tStrength:\t%f\n", ambient->strength);
 	printf("\tColor:\n");
-	printf("\t\tRed: %u\n", ambient->color.red);
-	printf("\t\tGreen: %u\n", ambient->color.green);
-	printf("\t\tBlue: %u\n", ambient->color.blue);
+	printf("\t\tRed:\t%u\n", ambient->color.red);
+	printf("\t\tGreen:\t%u\n", ambient->color.green);
+	printf("\t\tBlue:\t%u\n", ambient->color.blue);
 }
 
 static void	print_debug_sphere(t_sphere *sphere)
 {
 	printf("Sphere:\n");
-	printf("\tRadius: %f\n", sphere->radius);
+	printf("\tRadius:\t%f\n", sphere->radius);
 	printf("\tPosition:\n");
-	printf("\t\tX: %f\n", sphere->position.x);
-	printf("\t\tY: %f\n", sphere->position.y);
-	printf("\t\tZ: %f\n", sphere->position.z);
+	printf("\t\tX:\t%f\n", sphere->position.x);
+	printf("\t\tY:\t%f\n", sphere->position.y);
+	printf("\t\tZ:\t%f\n", sphere->position.z);
 	printf("\tColor:\n");
-	printf("\t\tRed: %u\n", sphere->color.red);
-	printf("\t\tGreen: %u\n", sphere->color.green);
-	printf("\t\tBlue: %u\n", sphere->color.blue);
+	printf("\t\tRed:\t%u\n", sphere->color.red);
+	printf("\t\tGreen:\t%u\n", sphere->color.green);
+	printf("\t\tBlue:\t%u\n", sphere->color.blue);
 }
 
 static void	print_debug_plane(t_plane *plane)
 {
 	printf("Plane:\n");
 	printf("\tPosition:\n");
-	printf("\t\tX: %f\n", plane->position.x);
-	printf("\t\tY: %f\n", plane->position.y);
-	printf("\t\tZ: %f\n", plane->position.z);
+	printf("\t\tX:\t%f\n", plane->position.x);
+	printf("\t\tY:\t%f\n", plane->position.y);
+	printf("\t\tZ:\t%f\n", plane->position.z);
 	printf("\tNormal:\n");
-	printf("\t\tX: %f\n", plane->normal.x);
-	printf("\t\tY: %f\n", plane->normal.y);
-	printf("\t\tZ: %f\n", plane->normal.z);
+	printf("\t\tX:\t%f\n", plane->normal.x);
+	printf("\t\tY:\t%f\n", plane->normal.y);
+	printf("\t\tZ:\t%f\n", plane->normal.z);
 	printf("\tColor:\n");
-	printf("\t\tRed: %u\n", plane->color.red);
-	printf("\t\tGreen: %u\n", plane->color.green);
-	printf("\t\tBlue: %u\n", plane->color.blue);
+	printf("\t\tRed:\t%u\n", plane->color.red);
+	printf("\t\tGreen:\t%u\n", plane->color.green);
+	printf("\t\tBlue:\t%u\n", plane->color.blue);
 }
 
 static void	print_debug_cylinder(t_cylinder *cylinder)
 {
 	printf("Cylinder:\n");
-	printf("\tDiameter: %f\n", cylinder->diameter);
-	printf("\tHeight: %f\n", cylinder->height);
+	printf("\tDiameter:\t%f\n", cylinder->diameter);
+	printf("\tHeight:\t%f\n", cylinder->height);
 	printf("\tPosition:\n");
-	printf("\t\tX: %f\n", cylinder->position.x);
-	printf("\t\tY: %f\n", cylinder->position.y);
-	printf("\t\tZ: %f\n", cylinder->position.z);
+	printf("\t\tX:\t%f\n", cylinder->position.x);
+	printf("\t\tY:\t%f\n", cylinder->position.y);
+	printf("\t\tZ:\t%f\n", cylinder->position.z);
 	printf("\tOrientation:\n");
-	printf("\t\tX: %f\n", cylinder->orientation.x);
-	printf("\t\tY: %f\n", cylinder->orientation.y);
-	printf("\t\tZ: %f\n", cylinder->orientation.z);
+	printf("\t\tX:\t%f\n", cylinder->orientation.x);
+	printf("\t\tY:\t%f\n", cylinder->orientation.y);
+	printf("\t\tZ:\t%f\n", cylinder->orientation.z);
 	printf("\tColor:\n");
-	printf("\t\tRed: %u\n", cylinder->color.red);
-	printf("\t\tGreen: %u\n", cylinder->color.green);
-	printf("\t\tBlue: %u\n", cylinder->color.blue);
+	printf("\t\tRed:\t%u\n", cylinder->color.red);
+	printf("\t\tGreen:\t%u\n", cylinder->color.green);
+	printf("\t\tBlue:\t%u\n", cylinder->color.blue);
 }
 
 void	print_debug(void *comp, t_component_type type)
@@ -114,4 +114,20 @@ void	print_debug(void *comp, t_component_type type)
 		print_debug_plane((t_plane *)comp);
 	else if (type == COMP_CYLINDER) //cylinder
 		print_debug_cylinder((t_cylinder *)comp);
+}
+
+static double	get_frame_time(t_timer *timer)
+{
+	double	time;
+
+	time = (timer->end.tv_sec - timer->start.tv_sec) * 1000.0;
+	time += (timer->end.tv_usec - timer->start.tv_usec) / 1000.0;
+	return (time);
+}
+
+void	display_debug_time(t_rt *rt)
+{
+	char str[100];
+	sprintf(str, "Frame time: %.3fms", get_frame_time(&rt->timer));
+	mlx_string_put(rt->mlx.mlx, rt->mlx.win.win, 10, 20, 0xFFFFFF, str);
 }
