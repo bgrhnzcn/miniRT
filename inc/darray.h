f15b59d5d23f9a177458dcf71d6dec4771b7c489
bgrhnzcn <bugrahanozcan16@gmail.com>
2024-11-25 00:19:46 +0300

    Removed unnecessart y type_size in functions.

--- inc/darray.h
+++ inc/darray.h
@@ -6,20 +6,25 @@
 /*   By: bgrhnzcn <bgrhnzcn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 16:34:51 by bgrhnzcn          #+#    #+#             */
-/*   Updated: 2024/11/24 16:50:01 by bgrhnzcn         ###   ########.fr       */
+/*   Updated: 2024/11/24 19:32:36 by bgrhnzcn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#ifndef DARRAY_H
+# define DARRAY_H
+
+# include "libft.h"
 
 typedef struct s_darray
 {
 	void	*data;
-	size_t	type_size;
-	size_t	capacity;
-	size_t	size;
+	int		type_size;
+	int		capacity;
+	int		size;
 }	t_darray;
 
-t_darray	*init_darray(size_t capacity, size_t type_size);
-int			push_darray(t_darray *darray, void *data, size_t type_size);
+t_darray	*init_darray(int capacity, int type_size);
+int			push_darray(t_darray *darray, void *data);
 void		free_darray(t_darray *darray);
+
+#endif
--- src/darray.c
+++ src/darray.c
@@ -6,20 +6,20 @@
 /*   By: bgrhnzcn <bgrhnzcn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 16:39:23 by bgrhnzcn          #+#    #+#             */
-/*   Updated: 2024/11/24 16:50:03 by bgrhnzcn         ###   ########.fr       */
+/*   Updated: 2024/11/24 18:19:10 by bgrhnzcn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "darray.h"
 
-t_darray	*init_darray(size_t capacity, size_t type_size)
+t_darray	*init_darray(int capacity, int type_size)
 {
 	t_darray	*darray;
 
 	darray = (t_darray *)malloc(sizeof(t_darray));
 	if (!darray)
 		return (NULL);
-	darray->data = malloc(capacity * type_size);
+	darray->data = ft_calloc(capacity, type_size);
 	if (!darray->data)
 		return (free(darray), NULL);
 	darray->type_size = type_size;
@@ -28,19 +28,21 @@ t_darray	*init_darray(size_t capacity, size_t type_size)
 	return (darray);
 }
 
-int	push_darray(t_darray *darray, void *data, size_t type_size)
+int	push_darray(t_darray *darray, void *data)
 {
 	void	*tmp;
 
 	if (darray->size == darray->capacity)
 	{
 		tmp = darray->data;
-		darray->data = malloc(darray->capacity * 2 * type_size);
+		darray->capacity = 2 * darray->capacity;
+		darray->data = malloc(darray->capacity * darray->type_size);
 		if (!darray->data)
 			return (free(tmp), 1);
-		ft_memcpy(darray->data, tmp, darray->size * type_size);
+		ft_memcpy(darray->data, tmp, darray->size * darray->type_size);
 	}
-	ft_memcpy(darray->data + (darray->size * type_size), data, type_size);
+	ft_memcpy(darray->data + (darray->size * darray->type_size),
+		data, darray->type_size);
 	darray->size++;
 	return (0);
 }
