a2c6b7822e9aa111b09899fa099c04e258524f08
bgrhnzcn <bugrahanozcan16@gmail.com>
2024-11-24 16:09:21 +0300

    Initialize rendering struct and enhance input handling with key press and release events.

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@ int main(int ac, char *av[])
 {
 	t_rt	rt;
 
+	ft_memset(&rt, 0, sizeof(t_rt));
 	if (ac != 2)
 		return (printf("./miniRT <path-to-scene>\n"));
 	else if (control_extension(av[1]) == EXIT_FAILURE)
@@ -11,7 +12,10 @@ int main(int ac, char *av[])
 	read_rt(av[1]);
 	if (init_display(&rt.mlx))
 		return (printf("Failed to initialize display.\n"));
-	mlx_hook(rt.mlx.win.win, 17, 0, terminate_program, &rt);
+	mlx_do_key_autorepeatoff(rt.mlx.mlx);
+	mlx_hook(rt.mlx.win.win, Destroy, DestroyMask, terminate_program, &rt);
+	mlx_hook(rt.mlx.win.win, KeyPress, KeyPressMask, key_press, &rt);
+	mlx_hook(rt.mlx.win.win, KeyRelease, KeyRelease, key_release, &rt);
 	mlx_loop_hook(rt.mlx.mlx, update_frame, &rt);
 	mlx_loop(rt.mlx.mlx);
 }
