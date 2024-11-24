eebf6f9e3d65d3ddc9929ca787aae273efcad610
bgrhnzcn <bugrahanozcan16@gmail.com>
2024-11-24 16:07:04 +0300

    Add input handling for program termination in update function

--- src/update.c
+++ src/update.c
@@ -6,14 +6,21 @@
 /*   By: bgrhnzcn <bgrhnzcn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 00:50:17 by bgrhnzcn          #+#    #+#             */
-/*   Updated: 2024/11/24 01:11:14 by bgrhnzcn         ###   ########.fr       */
+/*   Updated: 2024/11/24 16:06:47 by bgrhnzcn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+void	update_inputs(t_rt *rt)
+{
+	if (rt->key_states[ESC_KEY] && rt->key_states[CTRL_KEY])
+		terminate_program(rt);
+}
+
 int	update_frame(t_rt *rt)
 {
+	update_inputs(rt); //Updates inputs
 	gettimeofday(&rt->timer.start, NULL); //Frame start time
 	render(rt); //Starting point for rendering.
 	gettimeofday(&rt->timer.end, NULL); // Frame end time
